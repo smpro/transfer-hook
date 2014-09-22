1f799311219a57985b4079e7e3c18497d8c7f98f
Jan Veverka <jan.veverka@cern.ch>
2014-09-22 17:33:46 +0200

    Only transfers  lumis that are not yet in the DB

--- watchAndInject.py
+++ watchAndInject.py
@@ -11,6 +11,9 @@ import shutil
 hltkeysscript = "/opt/transferTests/hltKeyFromRunInfo.pl"
 injectscript = "/opt/transferTests/injectFileIntoTransferSystem.pl"
 #injectscript = "/opt/transferTests/testParams.sh"
+_streams_to_ignore = ['EventDisplay', 'DQMHistograms']
+_run_number_min = 226485
+_run_number_max = 300000
 
 def get_runs_and_hltkey(path, hltkeys):
     
@@ -41,10 +44,6 @@ def watch_and_inject(path):
     #runs = get_runs_and_hltkey(path, hltkeys)
 
 
-    #print hltkeys
-    #for run in runs:
-    #for run in ["/store/lustre/mergeMacro/run224074", "/store/lustre/mergeMacro/run224080", "/store/lustre/mergeMacro/run224148", "/store/lustre/mergeMacro/run224187", "/store/lustre/mergeMacro/run224230", "/store/lustre/mergeMacro/run224236", "/store/lustre/mergeMacro/run224238", "/store/lustre/mergeMacro/run224241", "/store/lustre/mergeMacro/run224244", "/store/lustre/mergeMacro/run224259", "/store/lustre/mergeMacro/run224273", "/store/lustre/mergeMacro/run224286", "/store/lustre/mergeMacro/run224307", "/store/lustre/mergeMacro/run224324", "/store/lustre/mergeMacro/run224326", "/store/lustre/mergeMacro/run224328", "/store/lustre/mergeMacro/run224340", "/store/lustre/mergeMacro/run224352", "/store/lustre/mergeMacro/run224359", "/store/lustre/mergeMacro/run224371", "/store/lustre/mergeMacro/run224373", "/store/lustre/mergeMacro/run224374", "/store/lustre/mergeMacro/run224375", "/store/lustre/mergeMacro/run224376", "/store/lustre/mergeMacro/run224379", "/store/lustre/mergeMacro/run224380", "/store/lustre/mergeMacro/run224393", "/store/lustre/mergeMacro/run224397", "/store/lustre/mergeMacro/run224409", "/store/lustre/mergeMacro/run224412", "/store/lustre/mergeMacro/run224413", "/store/lustre/mergeMacro/run224417", "/store/lustre/mergeMacro/run224440", "/store/lustre/mergeMacro/run224457", "/store/lustre/mergeMacro/run224471", "/store/lustre/mergeMacro/run224481", "/store/lustre/mergeMacro/run224482", "/store/lustre/mergeMacro/run224483", "/store/lustre/mergeMacro/run224484", "/store/lustre/mergeMacro/run224497", "/store/lustre/mergeMacro/run224499", "/store/lustre/mergeMacro/run224502", "/store/lustre/mergeMacro/run224506", "/store/lustre/mergeMacro/run224512"]: 
-
     # MWGR4 good runs: 225075, 225080, 225115, 225117, 225119, 225125
     mwgr4runs = [225075, 225080, 225115, 225117, 225119, 225125]
     mwgr5runs1 = [225709, 225713]
@@ -52,10 +51,12 @@ def watch_and_inject(path):
                   225849, 225860, 225861, 225862, 225893, 225896,
                   225904, 225906, 225909, 225910, 225916, 225918, 
                   225919, 225930, 225948, 225949, 225953, 225956, ]
-    runs_to_transfer = mwgr5runs2
-    for runNumber in runs_to_transfer[:]:
+    mwgr7runs = [226485,]
+    runs_to_transfer = mwgr7runs
+    for runNumber in runs_to_transfer[:2]:
         runNumber = '%d' % runNumber
         run = "/store/lustre/mergeMacro/run" + runNumber
+        # run = "/store/lustre/oldMergeMacro/run" + runNumber
         #try:
         #    os.mkdir(run + "/transferred")
         #except OSError, e:
@@ -73,8 +74,8 @@ def watch_and_inject(path):
             hltkeys[runNumber] = out.strip()
 
         jsns = glob.glob(run + '/*jsn')
-        for jsn_file in jsns:
-            if "streamError" not in  jsn_file:
+        for jsn_file in jsns[:100]:
+            if "streamError" not in jsn_file:
                 settings_textI = open(jsn_file, "r").read()
                 settings = json.loads(settings_textI)
                 eventsNumber = int(settings['data'][1])
@@ -83,11 +84,12 @@ def watch_and_inject(path):
                 lumiSection = int(fileName.split('_')[1].strip('ls'))
                 #streamName = str(fileName.split('_')[2].strip('stream'))
                 streamName = str(fileName.split('_')[2].split('stream')[1])
+                if streamName in _streams_to_ignore:
+                    continue
 
                 #call the actual inject script
                 if eventsNumber != 0:
 
-                    #prepare for GR4 --> CMSSW version updated
                     args_transfer = [injectscript   ,
                             '--filename'   , fileName,
                             "--path"       , run,
@@ -96,7 +98,7 @@ def watch_and_inject(path):
                             "--lumisection",  str(lumiSection),
                             "--numevents"  , str(eventsNumber),
                             "--appname"    , "CMSSW",
-                            "--appversion" , "CMSSW_7_1_6",
+                            "--appversion" , "CMSSW_7_1_9_patch1",
                             "--stream"     , streamName,
                             "--setuplabel" , "Data",
                             "--config"     , "/opt/injectworker/.db.conf",
@@ -108,7 +110,7 @@ def watch_and_inject(path):
                     args_check = [injectscript, '--check'             ,
                             '--filename', fileName                    ,
                             "--config"  , "/opt/injectworker/.db.conf",]
-                    args = args_transfer
+                    args = args_check
                     print "I'll run:\n", ' '.join(args)
                     p = subprocess.Popen(args, stdout=subprocess.PIPE,
                                          stderr=subprocess.PIPE)
@@ -116,15 +118,22 @@ def watch_and_inject(path):
                     out, err = p.communicate()
                     print out
                     print err
+                    if 'File not found in database.' in out:
+                        args = args_transfer
+                        print 'Ready to transfer', jsn_file
+                        print "I'll run:\n", ' '.join(args)
+                        p = subprocess.Popen(args, stdout=subprocess.PIPE,
+                                             stderr=subprocess.PIPE)
+    
+                        out, err = p.communicate()
+                        print out
+                        print err
                     #if "File sucessfully submitted for transfer" in out:
                         #shutil.move(jsn_file,run + "/transferred/" + os.path.basename(jsn_file))
                         #shutil.move(run + fileName, run + "/transferred/" + fileName)
                     #else:
                     #    print "I've encountered some error:\n", out
         
-
-
-
 if __name__ == '__main__':
     parser = OptionParser(usage="usage: %prog [-h|--help] [-p|--path]")
     parser.add_option("-p", "--path",
