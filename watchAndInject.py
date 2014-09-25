8ef584d78dcf53a51f3990c3a42d9c44463d4444
Jan Veverka <jan.veverka@cern.ch>
2014-09-25 16:44:56 +0200

    Factored out calls to shell to a function.

--- watchAndInject.py
+++ watchAndInject.py
@@ -31,18 +31,15 @@ import shutil
 import pprint
 import time
 
-hltkeysscript = "/opt/transferTests/hltKeyFromRunInfo.pl"
-injectscript = "/opt/transferTests/injectFileIntoTransferSystem.pl"
-#injectscript = "/opt/transferTests/testParams.sh"
-## EventDisplay and DQMHistograms should not be transferred
-## DQM should be transferred but it's taken out because it causes 
-## problems
+_hltkeysscript = "/opt/transferTests/hltKeyFromRunInfo.pl"
+_injectscript = "/opt/transferTests/injectFileIntoTransferSystem.pl"
 _streams_to_ignore = ['EventDisplay', 'DQMHistograms', 'DQM']
 _run_number_min = 226673
 _run_number_max = 300000
+_CMSSW_version_old = 'CMSSW_7_1_9_patch1'
+_CMSSW_version_new = 'CMSSW_7_1_10'
 
 def get_runs_and_hltkey(path, hltkeys):
-    
     runs = []
     for run in glob.glob(path):
         runNumber = os.path.basename(run).replace('run', '')
@@ -51,14 +48,9 @@ def get_runs_and_hltkey(path, hltkeys):
             continue
         runs.append(run)
         if runNumber not in hltkeys.keys():
-     
-            args = [hltkeysscript, '-r', runNumber]
-            log("I'll run:\n  %s" % ' '.join(args))
-            p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            out, err = p.communicate()
-            #print out, err
+            args = [_hltkeysscript, '-r', runNumber]
+            out, err = log_and_exec(args)
             if err:
-                #print "no hltkey associated with the run", runNumber, ", setting it to UNKOWN"
                 hltkeys[runNumber] = "UNKNOWN"
             else:
                 hltkeys[runNumber] = out.strip()
@@ -67,13 +59,7 @@ def get_runs_and_hltkey(path, hltkeys):
 
 
 def watch_and_inject(path):
-    
     hltkeys = dict()
-  
-    #put everything in an infinite loop if we want to transfer everything - too much junk for now
-    #while True:
-
-    #this gets all the hltkeys, useful only if the watched path is clean - and we actually loop through all the existing runs
     runs_to_transfer = get_runs_and_hltkey(path, hltkeys)
     log('Runs to transfer: ', newline=False) 
     pprint.pprint(runs_to_transfer)
@@ -117,12 +103,20 @@ def watch_and_inject(path):
                 #call the actual inject script
                 if eventsNumber != 0:
 
-                    args_transfer = [injectscript   ,
+
+                    #this is to check the status of the files
+                    args = [_injectscript, '--check'                   ,
+                            '--filename', fileName                    ,
+                            "--config"  , "/opt/injectworker/.db.conf",]
+                    out, err = log_and_exec(args, print_output=True)
+                    if 'File not found in database.' in out:
+                        print 'Ready to transfer', jsn_file
+                        args = [_injectscript,
                             '--filename'   , fileName,
                             "--path"       , run,
                             "--type"       , "streamer",
                             "--runnumber"  , runNumber,
-                            "--lumisection",  str(lumiSection),
+                            "--lumisection", str(lumiSection),
                             "--numevents"  , str(eventsNumber),
                             "--appname"    , "CMSSW",
                             "--appversion" , "CMSSW_7_1_9_patch1",
@@ -132,35 +126,22 @@ def watch_and_inject(path):
                             "--destination", "Global",
                             "--filesize"   , str(fileSize),
                             "--hltkey"     , hltkeys[runNumber]]
-
-                    #this is to check the status of the files
-                    args_check = [injectscript, '--check'             ,
-                            '--filename', fileName                    ,
-                            "--config"  , "/opt/injectworker/.db.conf",]
-                    args = args_check
-                    log("I'll run:\n  %s" % ' '.join(args))
-                    p = subprocess.Popen(args, stdout=subprocess.PIPE,
-                                         stderr=subprocess.PIPE)
-
-                    out, err = p.communicate()
-                    print out
-                    print err
-                    if 'File not found in database.' in out:
-                        args = args_transfer
-                        print 'Ready to transfer', jsn_file
-                        print "I'll run:\n", ' '.join(args)
-                        p = subprocess.Popen(args, stdout=subprocess.PIPE,
-                                             stderr=subprocess.PIPE)
-    
-                        out, err = p.communicate()
-                        print out
-                        print err
+                        out, err = log_and_exec(args, print_output=True)
                     #if "File sucessfully submitted for transfer" in out:
                         #shutil.move(jsn_file,run + "/transferred/" + os.path.basename(jsn_file))
                         #shutil.move(run + fileName, run + "/transferred/" + fileName)
                     #else:
                     #    print "I've encountered some error:\n", out
-        
+
+def log_and_exec(args, print_output=False):        
+    log("I'll run:\n  %s" % ' '.join(args))
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if print_output:
+        print out
+        print err
+    return out, err
+
 def log(msg, newline=True):
     msg = "%s: %s" % (strftime(), msg)
     if newline:
