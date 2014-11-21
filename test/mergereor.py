0a9126b4144d4f2347356a654968b8ce4f353b83
Jan Veverka <jan.veverka@cern.ch>
2014-11-21 10:49:19 +0100

    Minimalistic working version of using the isCompleteRun from the merger

--- test/mergereor.py
+++ test/mergereor.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 from merger.cmsDataFlowCleanUp import isCompleteRun
 
+run_dir = '/store/lustre/mergeMacro_TEST/run220013'
+
+complete = isCompleteRun(debug = 10,
+                         theInputDataFolder = run_dir,
+                         completeMergingThreshold = 1.0,
+                         outputEndName = 'foo')
+
+print "complete:", complete
