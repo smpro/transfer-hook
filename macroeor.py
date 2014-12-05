29f84880464c712c8142ac1155e7d01e94c717fb
Jan Veverka <jan.veverka@gmail.com>
2014-12-05 13:16:36 +0100

    Applied autopep8 to macroeor to remove tabs and long lines

--- macroeor.py
+++ macroeor.py
@@ -10,55 +10,62 @@ log = logging.getLogger(__name__)
 """
 Function to define if a run is complete
 """
-def is_run_complete(debug, theInputDataFolder, completeMergingThreshold, outputEndName):
 
-   if(outputEndName == ""):
-      outputEndName = socket.gethostname()
 
-   theRunNumber  = ""
+def is_run_complete(
+        debug,
+        theInputDataFolder,
+        completeMergingThreshold,
+        outputEndName):
 
-   inputDataFolderString = theInputDataFolder.split('/')	   
-   if inputDataFolderString[len(inputDataFolderString)-1] == '':
-     theRunNumber = inputDataFolderString[len(inputDataFolderString)-2]
-   else:
-     theRunNumber = inputDataFolderString[len(inputDataFolderString)-1] 
+    if(outputEndName == ""):
+        outputEndName = socket.gethostname()
 
-   # reading the list of files in the given folder
-   after = dict ([(f, None) for f in os.listdir (theInputDataFolder)])
-   afterStringSM = [f for f in after]
+    theRunNumber = ""
 
-   numberMiniEoRFiles  = 0
-   eventsInputBUs      = 0
-   eventsInputFUs      = 0
-   numberBoLSFiles     = 0
-   eventsTotalRun      = 0
-   eventsIDict         = dict()
-   iniIDict            = dict()
+    inputDataFolderString = theInputDataFolder.split('/')
+    if inputDataFolderString[len(inputDataFolderString) - 1] == '':
+        theRunNumber = inputDataFolderString[len(inputDataFolderString) - 2]
+    else:
+        theRunNumber = inputDataFolderString[len(inputDataFolderString) - 1]
 
-   for nb in range(0, len(afterStringSM)):
-      if afterStringSM[nb].endswith(".ini"):
-         fileIniString = afterStringSM[nb].split('_')
-         key = (fileIniString[2])
-         if key in iniIDict.keys():
-	    iniIDict[key].append(fileIniString[3].split('.ini')[0])
-	 else:
-	    iniIDict.update({key:[fileIniString[3].split('.ini')[0]]})
+    # reading the list of files in the given folder
+    after = dict([(f, None) for f in os.listdir(theInputDataFolder)])
+    afterStringSM = [f for f in after]
 
-      if not afterStringSM[nb].endswith(".jsn"): continue
-      if "index" in afterStringSM[nb]: continue
-      if afterStringSM[nb].endswith("recv"): continue
-      if "EoLS" in afterStringSM[nb]: continue
-      if "BoLS" in afterStringSM[nb]: continue
-      if "MacroEoR" in afterStringSM[nb]: continue
+    numberMiniEoRFiles = 0
+    eventsInputBUs = 0
+    eventsInputFUs = 0
+    numberBoLSFiles = 0
+    eventsTotalRun = 0
+    eventsIDict = dict()
+    iniIDict = dict()
 
-      inputEoRJsonFile = os.path.join(theInputDataFolder, afterStringSM[nb])
-      settingsLS = ""
-      if(os.path.getsize(inputEoRJsonFile) > 0):
-         try:
-            settingsLS_textI = open(inputEoRJsonFile, "r").read()
-            settingsLS = json.loads(settingsLS_textI)
-         except ValueError, e:
-            log.warning("Looks like the file {0} is not available, I'll try again...".format(inputEoRJsonFile))
+    for nb in range(0, len(afterStringSM)):
+        if afterStringSM[nb].endswith(".ini"):
+            fileIniString = afterStringSM[nb].split('_')
+            key = (fileIniString[2])
+            if key in iniIDict.keys():
+                iniIDict[key].append(fileIniString[3].split('.ini')[0])
+            else:
+                iniIDict.update({key: [fileIniString[3].split('.ini')[0]]})
+
+        if not afterStringSM[nb].endswith(".jsn"):
+            continue
+        if "index" in afterStringSM[nb]:
+            continue
+        if afterStringSM[nb].endswith("recv"):
+            continue
+        if "EoLS" in afterStringSM[nb]:
+            continue
+        if "BoLS" in afterStringSM[nb]:
+            continue
+        if "MacroEoR" in afterStringSM[nb]:
+            continue
+
+        inputEoRJsonFile = os.path.join(theInputDataFolder, afterStringSM[nb])
+        settingsLS = ""
+        if(os.path.getsize(inputEoRJsonFile) > 0):
             try:
                time.sleep (0.1)
                settingsLS_textI = open(inputEoRJsonFile, "r").read()
@@ -115,55 +122,69 @@ def is_run_complete(debug, theInputDataFolder, completeMergingThreshold, outputE
          except ValueError, e:
             log.warning("Looks like the file {0} is not available, I'll try again...".format(inputBadJsonFile))
             try:
-               time.sleep (0.1)
-               settingsLS_textI = open(inputBadJsonFile, "r").read()
-               settingsLS = json.loads(settingsLS_textI)
-            except ValueError, e:
-               log.warning("Looks like the file {0} is not available (2nd try)...".format(inputBadJsonFile))
-               time.sleep (1.0)
-               settingsLS_textI = open(inputBadJsonFile, "r").read()
-               settingsLS = json.loads(settingsLS_textI)
+                settingsLS_textI = open(inputBadJsonFile, "r").read()
+                settingsLS = json.loads(settingsLS_textI)
+            except ValueError as e:
+                log.warning("Looks like the file {0} is not available, I'll try again...".format(
+                    inputBadJsonFile))
+                try:
+                    time.sleep(0.1)
+                    settingsLS_textI = open(inputBadJsonFile, "r").read()
+                    settingsLS = json.loads(settingsLS_textI)
+                except ValueError as e:
+                    log.warning(
+                        "Looks like the file {0} is not available (2nd try)...".format(inputBadJsonFile))
+                    time.sleep(1.0)
+                    settingsLS_textI = open(inputBadJsonFile, "r").read()
+                    settingsLS = json.loads(settingsLS_textI)
 
-         eventsInput = int(settingsLS["data"][0])
-         # 0: run, 1: ls, 2: stream
-         fileNameString = afterStringBad[nb].split('_')
-         key = (fileNameString[2])
-         if key in eventsBadDict.keys():
+            eventsInput = int(settingsLS["data"][0])
+            # 0: run, 1: ls, 2: stream
+            fileNameString = afterStringBad[nb].split('_')
+            key = (fileNameString[2])
+            if key in eventsBadDict.keys():
 
-	    eventsInput = eventsBadDict[key][0] + eventsInput
-	    eventsBadDict[key].remove(eventsBadDict[key][0])
-	    eventsBadDict.update({key:[eventsInput]})
+                eventsInput = eventsBadDict[key][0] + eventsInput
+                eventsBadDict[key].remove(eventsBadDict[key][0])
+                eventsBadDict.update({key: [eventsInput]})
 
-	 else:
-	    eventsBadDict.update({key:[eventsInput]})	    
+            else:
+                eventsBadDict.update({key: [eventsInput]})
 
+    # Analyzing the information
+    isComplete = True
+    for streamName in eventsIDict:
+        if "DQM" in streamName:
+            continue
+        if "streamError" in streamName:
+            continue
+        sumEvents = eventsIDict[streamName][0]
+        if streamName in eventsBadDict:
+            sumEvents = sumEvents + eventsBadDict[streamName][0]
+        if(sumEvents < eventsInputBUs * completeMergingThreshold):
+            isComplete = False
 
-   # Analyzing the information
-   isComplete = True
-   for streamName in eventsIDict:
-      if "DQM" in streamName: continue
-      if "streamError" in streamName: continue
-      sumEvents = eventsIDict[streamName][0]
-      if streamName in eventsBadDict:
-         sumEvents = sumEvents + eventsBadDict[streamName][0]
-      if(sumEvents < eventsInputBUs*completeMergingThreshold):
-         isComplete = False
+    if(float(debug) >= 10):
+        print "run/events/completion: ", theInputDataFolder, eventsInputBUs, eventsInputFUs, numberBoLSFiles, isComplete
+    if(float(debug) >= 10 and 'streamA' in iniIDict.keys()):
+        print "numberMiniEoRFiles/streamAfile: ", numberMiniEoRFiles, len(iniIDict["streamA"])
 
-   if(float(debug) >= 10): print "run/events/completion: ",theInputDataFolder,eventsInputBUs,eventsInputFUs,numberBoLSFiles,isComplete
-   if(float(debug) >= 10 and 'streamA' in iniIDict.keys()): print "numberMiniEoRFiles/streamAfile: ",numberMiniEoRFiles,len(iniIDict["streamA"])
+    EoRFileNameMacroOutput = theInputDataFolder + "/" + \
+        theRunNumber + "_ls0000_MacroEoR_" + outputEndName + ".jsn_TEMP"
+    EoRFileNameMacroOutputStable = theInputDataFolder + "/" + \
+        theRunNumber + "_ls0000_MacroEoR_" + outputEndName + ".jsn"
 
-   EoRFileNameMacroOutput	= theInputDataFolder + "/" + theRunNumber + "_ls0000_MacroEoR_" + outputEndName + ".jsn_TEMP"
-   EoRFileNameMacroOutputStable = theInputDataFolder + "/" + theRunNumber + "_ls0000_MacroEoR_" + outputEndName + ".jsn"
+    theEoRFileMacroOutput = open(EoRFileNameMacroOutput, 'w')
+    theEoRFileMacroOutput.write(
+        json.dumps(
+            {
+                'eventsInputBUs': eventsInputBUs,
+                'eventsInputFUs': eventsInputFUs,
+                'eventsStreamInput': eventsIDict,
+                'eventsStreamBadInput': eventsBadDict,
+                'numberBoLSFiles': numberBoLSFiles,
+                'eventsTotalRun': eventsTotalRun,
+                'isComplete': isComplete}))
+    theEoRFileMacroOutput.close()
 
-   theEoRFileMacroOutput = open(EoRFileNameMacroOutput, 'w')
-   theEoRFileMacroOutput.write(json.dumps({'eventsInputBUs':       eventsInputBUs, 
-   					   'eventsInputFUs':       eventsInputFUs, 
-   					   'eventsStreamInput':    eventsIDict, 
-   					   'eventsStreamBadInput': eventsBadDict, 
-     					   'numberBoLSFiles':      numberBoLSFiles,
-					   'eventsTotalRun':       eventsTotalRun,
-					   'isComplete':           isComplete}))
-   theEoRFileMacroOutput.close()
-
-   shutil.move(EoRFileNameMacroOutput, EoRFileNameMacroOutputStable)
-
+    shutil.move(EoRFileNameMacroOutput, EoRFileNameMacroOutputStable)
