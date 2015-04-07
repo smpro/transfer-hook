f8b893e1d8361c2a91dc9180944ca7e7f68b46c2
Jan Veverka <jan.veverka@cern.ch>
2015-04-07 14:32:54 +0200

    Search for DB config in $HOME/.sm if none provided in $SM_DB_CONF

--- smops/check-run.sh
+++ smops/check-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-## USAGE: sudo ./fix_missing_streamers.sh
+## USAGE: sudo ./check-run.sh
+if [ ! -f "$SM_DB_CONF" ]; then
+    SM_DB_CONF=$HOME/.sm/.db.conf
+fi
 PERL5LIB=/usr/lib/perl5/vendor_perl/5.8.8 \
 LD_LIBRARY_PATH=/usr/lib/oracle/10.2.0.3/client/lib \
 ~smpro/scripts/checkRun.pl \
