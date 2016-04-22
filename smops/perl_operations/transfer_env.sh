eb315e0281aeb0137ca4c067670a162039d9d5c8
Zeynep <zeynep.demiragli@gmail.com>
2016-04-22 10:27:57 +0200

    adding perl automatic scripts

---
+++ smops/perl_operations/transfer_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# t0_env  sets perl environment for some of the scripts
+#
+
+#export T0_BASE_DIR="/nfshome0/cmsprod/TransferTest"
+#export T0ROOT=/nfshome0/cmsprod/TransferTest/T0
+#export PERL5LIB=${T0ROOT}/perl_lib:${T0_BASE_DIR}/ApMon_perl-2.2.6:${T0_BASE_DIR}/perl
+
+export PERL5LIB=/opt/copymanager/perl_lib
+
