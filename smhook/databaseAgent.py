0ad923bdce96231f625e190838b1e97c37bef142
Dylan Hsu <endlessnumber@yahoo.com>
2016-09-16 16:18:52 +0200

    Use signal.setitimer for better timeout resolution (ms) and don't retry indefinitely

--- smhook/databaseAgent.py
+++ smhook/databaseAgent.py
@@ -16,12 +16,12 @@ import smhook.config
 # Hardcoded Config file to be used, is defined below:
 # We read from production DB no matter what (in either case)
 # but for testing, write to integration DB only
-debug=False
-#myconfig = os.path.join(smhook.config.DIR, '.db_rates_integration.py')
-myconfig = os.path.join(smhook.config.DIR, '.db_rates_production.py')
+debug=True
+myconfig = os.path.join(smhook.config.DIR, '.db_rates_integration.py')
+#myconfig = os.path.join(smhook.config.DIR, '.db_rates_production.py')
 cxn_timeout = 60*60 # Timeout for database connection in seconds
 num_retries = 5
-query_timeout = 1.5 #Timeout for individual queries in seconds
+query_timeout = 2 #Timeout for individual queries in seconds
 
 logger = logging.getLogger(__name__)
 # For debugging purposes, initialize the logger to stdout if running script as a standalone
@@ -67,8 +67,8 @@ def useConnection(cxn_name):
     global cxn_exists, cxn_db, cxn_timestamp, cxn_names
     if cxn_name not in cxn_names:
         return False
-    is_fresh_cxn = int(time.time()) - cxn_timestamp[cxn_name]
-    if not cxn_exists[cxn_name] or not is_fresh_cxn[cxn_name]:
+    fresh_cxn = int(time.time()) - cxn_timestamp[cxn_name] <  cxn_timeout
+    if not cxn_exists[cxn_name] or not fresh_cxn:
         if cxn_exists:
             cxn_db[cxn_name].close()
             cxn_exists[cxn_name]=False
@@ -83,7 +83,7 @@ def useConnection(cxn_name):
                 logger.debug('Successfully reconnected to database "{0}"'.format(cxn_name))
             except cx_Oracle.DatabaseError as e:
                 error, = e.args
-                logger.error('Error connecting to database "{0}". Reason: {2}'.format(cxn_name, returnErrorMessage(error.code)))
+                logger.error('Error connecting to database "{0}". Reason: {1}'.format(cxn_name, returnErrorMessage(error.code)))
                 retries+=1
     if not cxn_exists[cxn_name]:
         return False
@@ -94,23 +94,30 @@ class TimeoutError(Exception):
     pass
 def timeoutHandler(signum, frame):
     raise TimeoutError()
-def timeout(func, args=(), kwargs={}, timeout_duration=1, default=None):
+def timeout(func, args=(), kwargs={}, timeout_duration=1, default=False):
     # set the timeout handler
-    signal.signal(signal.SIGALRM, timeoutHandler) 
-    signal.alarm(timeout_duration)
+    signal.signal(signal.SIGALRM, timeoutHandler) # Call timeoutHandler when the signal.SIGALRM is sent
+    signal.setitimer(signal.ITIMER_REAL, timeout_duration, 1) # Set interval timer to send a SIGALRM after timeout_duration and every second thereafter
     try:
         result = func(*args, **kwargs)
-    except TimeoutError as exc:
+    except TimeoutError as exc: # Raise exception if SIGALRM signal happens before we receive the query result
+        logger.error('Timeout {0} s exceeded calling function {1}'.format(timeout_duration, func.__name__))
         result = default
     finally:
-        logger.error('Timeout {0} s exceeded calling function {1}'.format(timeout_duration, func.__name__))
-        signal.alarm(0)
+        signal.setitimer(signal.ITIMER_REAL, 0) # Disable the alarm
     return result
 
-def query(cxn_name, query, custom_timeout=0):
-    #Interface for passing queries to the database agent
+def query(cxn_name, query, fetch_output=True, custom_timeout=0):
+    # databaseAgent.query
+    # Interface for passing queries to the database agent.
+    #
+    # cxn_name:         slug name for the connection to use
+    # query:            SQL statement to run
+    # fetch_output:     whether to try to fetch output (will cause an error if you try to fetch from an UPDATE statement)
+    # custom_timeout:   override default query timeout, unit is seconds
+
     logger.debug('Passing a query to database connection "{0}": "{1}"'.format(cxn_name, query.replace('\n', ' ').replace('\r', '')))
-    the_cxn = useConnection(cxn_name)
+    the_cxn = useConnection(cxn_name) # Get a fresh connection object
     if the_cxn==False:
         logger.error('Could not run query, unable to connect to "{0}"'.format(cxn_name))
         return False
@@ -118,18 +125,37 @@ def query(cxn_name, query, custom_timeout=0):
         the_timeout = custom_timeout
     else:
         the_timeout = query_timeout
-    args=[the_cxn, query]
+    args=[the_cxn, query, fetch_output] # Arguments to send to databaseAgent.executeQuery
     ran_query=False
     retries=0
     while ran_query==False and retries<num_retries:
-        result = timeout(executeQuery, args, timeout_duration=the_timeout, default=False)
+        logger.info('Try #{0} query on database "{1}": "{2}"'.format(retries+1, cxn_name, query))
+        try:
+            result = timeout(executeQuery, args, timeout_duration=the_timeout, default=False)
+            if result != False:
+                ran_query=True
+        except cx_Oracle.DatabaseError as e:
+            error, = e.args
+            logger.error('Error running query on database "{0}". Reason: {1}'.format(cxn_name, returnErrorMessage(error.code)))
+        except cx_Oracle.InterfaceError as e:
+            if e[0]=="not a query":
+                logger.error('databaseAgent requested output from non-query SQL statement')
+                result=True
+                ran_query=True
+            else:
+                logger.error("databaseAgent unknown Oracle interface error: {0}".format(e[0]))
+        retries+=1
     if result==False:
         return False
     else:
         return result
-def executeQuery(the_cxn, query):
-    #Internal function for actually executing the query
+def executeQuery(the_cxn, query, fetch_output=True):
+    # Internal function for actually executing the queries
+    # Very simple for now but may add some smart stuff later
     cursor=the_cxn.cursor()
     cursor.execute(query)
-    result=cursor.fetchall()
+    if fetch_output:
+        result=cursor.fetchall()
+    else:
+        result=True
     return result
