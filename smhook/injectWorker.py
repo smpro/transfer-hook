c0d997c0c5ea3f03601eb819f1c8ef337f4e9d4b
Dylan Hsu <endlessnumber@yahoo.com>
2016-11-17 17:30:27 +0100

    update python injectWorker to use bitwise status flags, still needs testing

--- smhook/injectWorker.py
+++ smhook/injectWorker.py
@@ -35,23 +35,23 @@ def insertFile(filename, runnumber, stream, ls):
         stream,
         filename,
         "TO_TIMESTAMP('"+str(datetime.datetime.utcnow())+"','YYYY-MM-DD HH24:MI:SS.FF6')",
-        "(SELECT STATUS_FLAG FROM FILE_STATUS_FLAGS WHERE MEANING='P5_OPENED')"
+        1
       )
     databaseAgent.runQuery('file_status', query, false)
     return true
 def closedFile(filename):
     # Mark as closed a file that already exists in 'open' state in the database
-    return __updateFile(filename, 'P5_OPENED', 'P5_CLOSED')
+    return __updateFile(filename, 2 ** 0, 2 ** 1)
 def copiedFile(filename):
     # Mark a file as transferred after successfully copying it
-    return __updateFile(filename, 'T0_INJECTED', 'TRANSFERRED')
+    return __updateFile(filename, 2 ** 2, 2 ** 3)
 def deletedFile(filename, repacked=True)
     # Mark a file as deleted after it has been repacked or checked (depending on the deletion policy)
     if repacked:
-        old_flag='P5_REPACKED'
+        old_flag = 2 ** 5
     else:
-        old_flag='P5_CHECKED'
-    return __updateFile(filename, old_flag, 'P5_DELETED')
+        old_flag = 2 ** 4
+    return __updateFile(filename, old_flag, 2 ** 6)
 def getInjectedFiles(stream_blacklist=[], runnumber_blacklist=[]):
     result=__pollT0ForFiles('T0_INJECTED', stream_blacklist, runnumber_blacklist)
     return result
@@ -62,33 +62,35 @@ def getRepackedFiles(stream_blacklist=[], runnumber_blacklist=[]):
     result=__pollT0ForFiles('T0_REPACKED', stream_blacklist, runnumber_blacklist)
     return result
 def __updateFile(filename, old_flag, new_flag):
-    # Update a file that is already in the database
+    # Update a file that is already in the database marked with old_flag
+	# See if the row is there
     query = ("SELECT COUNT(*) FROM CMS_STOMGR.FILE_TRANSFER_STATUS WHERE "+\
-      "STATUS_FLAG={0) AND "+\
-      "FILENAME='{1}'").format(
-        "(SELECT STATUS_FLAG FROM FILE_STATUS_FLAGS WHERE MEANING='{0}')".format(old_flag),
+      "BITAND(STATUS_FLAG,{0})={0}) AND "+\
+      "FILENAME='{1}'").format(old_flag),
         filename
       )
     result=databaseAgent.runQuery('file_status', query, true)
     if result[0][0] == 0:
         return false
+    # Update the row using a crude bitwise OR 
     query = ("UPDATE CMS_STOMGR.FILE_TRANSFER_STATUS "+\
-      "SET STATUS_FLAG={0} WHERE "+\
-      "STATUS_FLAG={1} AND "+\
+      "SET STATUS_FLAG=(256-BITAND(256-{0},256-STATUS_FLAG)) WHERE "+\
+      "BITAND(STATUS_FLAG,{1})={1} AND "+\
       "FILENAME='{2}'").format(
-        "(SELECT STATUS_FLAG FROM FILE_STATUS_FLAGS WHERE MEANING='{0}')".format(new_flag),
-        "(SELECT STATUS_FLAG FROM FILE_STATUS_FLAGS WHERE MEANING='{0}')".format(old_flag),
+        new_flag,
+        old_flag,
         filename
       )
     databaseAgent.runQuery('file_status', query, false)
     return true
-def __pollT0ForFiles(flag, stream_blacklist=[], runnumber_blacklist=[], operator="="):
+def __pollT0ForFiles(search_flag, unmarked_flag, stream_blacklist=[], runnumber_blacklist=[]):
+    # Find rows in the T0 table with the search flag marked and the unmarked flag not marked
     query = "SELECT FILENAME FROM CMS_STOMGR.FILE_TRANSFER_STATUS WHERE"+\
-      "STATUS_FLAG {0} (SELECT STATUS_FLAG FROM FILE_STATUS_FLAGS WHERE MEANING='{1}')".format(operator, flag)
+      "BITAND(STATUS_FLAG,{0})={0} AND BITAND(STATUS_FLAG,{1})=0".format(search_flag,unmarked_flag)
     if len(stream_blacklist) > 0:
         query += " AND STREAM NOT IN ('" + "', '".join(stream_blacklist) + "')"
     if len(runnumber_blacklist) > 0:
         query += " AND RUNNUMBER NOT IN (" + ", ".join(runnumber_blacklist) + ")"
-    result=database.Agent.runQuery('file_status_T0', query, true)
+    result=databaseAgent.runQuery('file_status_T0', query, true)
     return result
 
