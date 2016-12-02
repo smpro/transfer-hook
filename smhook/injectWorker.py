ab1a3b7e1f938308b9342f088dbf8feb83981a83
Dylan Hsu <endlessnumber@yahoo.com>
2016-12-02 16:24:20 +0100

    fix bugs after testing some injectWorker functions, remove the old ones from previous stage

--- smhook/injectWorker.py
+++ smhook/injectWorker.py
@@ -58,7 +58,7 @@ def insertFile(filename, runnumber, ls, stream, checksum, inject_into_T0=True):
       "DBMS_OUTPUT.PUT_LINE(ID_VAL); "+\
     "END;"
     query=query.format(runnumber, ls, stream, filename, checksum, 1, inject_flag, 0, "TO_TIMESTAMP('"+str(datetime.datetime.utcnow())+"','YYYY-MM-DD HH24:MI:SS.FF6')")
-    print query
+    #print query
     result = databaseAgent.runQuery('file_status', query, False)
     cursor.callproc("dbms_output.get_line", (lineVar, statusVar))
     got_file_id = statusVar.getvalue()==0
@@ -74,7 +74,7 @@ def recordTransferStart(file_id):
     query="UPDATE CMS_STOMGR.FILE_TRANSFER_STATUS "+\
       "SET TRANSFER_START_TIME={0} "+\
       "WHERE FILE_ID={1}"
-    query.format("TO_TIMESTAMP('"+str(datetime.datetime.utcnow())+"','YYYY-MM-DD HH24:MI:SS.FF6')", file_id)
+    query=query.format("TO_TIMESTAMP('"+str(datetime.datetime.utcnow())+"','YYYY-MM-DD HH24:MI:SS.FF6')", file_id)
     result = databaseAgent.runQuery('file_status', query, fetch_output=False)
     return result
 #______________________________________________________________________________
@@ -83,7 +83,7 @@ def recordTransferComplete(file_id):
       "SET TRANSFER_END_TIME = {0}, "+\
       "STATUS_FLAG = (255 - BITAND(255 - {1}, 255 - STATUS_FLAG))"+\
       "WHERE FILE_ID={2}"
-    query.format("TO_TIMESTAMP('"+str(datetime.datetime.utcnow())+"','YYYY-MM-DD HH24:MI:SS.FF6')", status_flags['TRANSFERRED'], file_id)
+    query=query.format("TO_TIMESTAMP('"+str(datetime.datetime.utcnow())+"','YYYY-MM-DD HH24:MI:SS.FF6')", status_flags['TRANSFERRED'], file_id)
     result = databaseAgent.runQuery('file_status', query, fetch_output=False)
     return result
 #______________________________________________________________________________
@@ -94,52 +94,10 @@ def recordCorruptedTransfer(file_id):
       "STATUS_FLAG = (255 - BITAND(255 - {1}, 255 - STATUS_FLAG)), "+\
       "BAD_CHECKSUM = 1 "+\
       "WHERE FILE_ID={2}"
-    query.format("TO_TIMESTAMP('"+str(datetime.datetime.utcnow())+"','YYYY-MM-DD HH24:MI:SS.FF6')", status_flags['TRANSFERRED'], file_id)
+    query=query.format("TO_TIMESTAMP('"+str(datetime.datetime.utcnow())+"','YYYY-MM-DD HH24:MI:SS.FF6')", status_flags['TRANSFERRED'], file_id)
     result = databaseAgent.runQuery('file_status', query, fetch_output=False)
     return result
 #______________________________________________________________________________
-def closedFile(filename):
-    # Mark as closed a file that already exists in 'open' state in the database
-    return __updateFile(filename, 2 ** 0, 2 ** 1)
-def copiedFile(filename):
-    # Mark a file as transferred after successfully copying it
-    return __updateFile(filename, 2 ** 2, 2 ** 3)
-def deletedFile(filename, repacked=True):
-    # Mark a file as deleted after it has been repacked or checked (depending on the deletion policy)
-    if repacked:
-        old_flag = 2 ** 5
-    else:
-        old_flag = 2 ** 4
-    return __updateFile(filename, old_flag, 2 ** 6)
-def getInjectedFiles(stream_blacklist=[], runnumber_blacklist=[]):
-    result=__pollT0ForFiles('T0_INJECTED', stream_blacklist, runnumber_blacklist)
-    return result
-def getCheckedFiles(stream_blacklist=[], runnumber_blacklist=[]):
-    result=__pollT0ForFiles('T0_CHECKED', stream_blacklist, runnumber_blacklist)
-    return result
-def getRepackedFiles(stream_blacklist=[], runnumber_blacklist=[]):
-    result=__pollT0ForFiles('T0_REPACKED', stream_blacklist, runnumber_blacklist)
-    return result
-def __updateFile(filename, old_flag, new_flag):
-    # Update a file that is already in the database marked with old_flag
-	# See if the row is there
-    query = ("SELECT COUNT(*) FROM CMS_STOMGR.FILE_TRANSFER_STATUS WHERE "+\
-      "BITAND(STATUS_FLAG,{0})={0}) AND "+\
-      "FILENAME='{1}'").format(old_flag, filename)
-    result=databaseAgent.runQuery('file_status', query, true)
-    if result[0][0] == 0:
-        return false
-    # Update the row using a crude bitwise OR 
-    query = ("UPDATE CMS_STOMGR.FILE_TRANSFER_STATUS "+\
-      "SET STATUS_FLAG=(255-BITAND(255-{0},255-STATUS_FLAG)) WHERE "+\
-      "BITAND(STATUS_FLAG,{1})={1} AND "+\
-      "FILENAME='{2}'").format(
-        new_flag,
-        old_flag,
-        filename
-      )
-    databaseAgent.runQuery('file_status', query, false)
-    return true
 def __pollT0ForFiles(search_flag, unmarked_flag, stream_blacklist=[], runnumber_blacklist=[]):
     # Find rows in the T0 table with the search flag marked and the unmarked flag not marked
     query = "SELECT FILENAME FROM CMS_STOMGR.FILE_TRANSFER_STATUS WHERE"+\
